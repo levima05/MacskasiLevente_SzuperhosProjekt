58f1617769d415762a9e1cef82859cf90d4d0148
Macskási Levente <levi.macskasi@gmail.com>
2024-11-08 12:24:37 +0100

    Done2

--- src/main/java/hu/petrik/szuperhosprojekt/Vasember.java
+++ src/main/java/hu/petrik/szuperhosprojekt/Vasember.java
@@ -12,6 +12,7 @@ public class Vasember extends Bosszuallo implements Milliardos {
     public void kutyutKeszit() {
         Random rand = new Random();
         float randomErtek = rand.nextFloat() * 10;
+
     }
 
     @Override
